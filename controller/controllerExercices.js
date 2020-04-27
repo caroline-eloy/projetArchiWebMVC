7fc7b188fbb2cc5883d92c4f917eec6b6a6a766a
Caroline <caroline@MBP-de-Caroline.lan>
2020-04-27 15:13:52 +0200

    update 3

--- controller/controllerExercices.js
+++ controller/controllerExercices.js
@@ -44,7 +44,28 @@ exports.exercices = function(req, res){
     exports.formulaireupdateexercice = function(req, res){
         console.log('renvoi formulaire');
         let idexercices = req.params.idexercices;
-        let sql = "SELECT * FROM exercices WHERE exercices.idexercices = ?";
+        let sql = "Select * from exercices WHERE exercices.`idexercices` = ? ";
+        connection.query(sql, idexercices, function (err, data)  {
+            if(err){
+                res.status(400).send(err);
+                console.log('erreur niveau formulaire modification')
+            }
+            else{
+                res.status(202);
+                console.log(data);
+                exercices = data;
+                res.render('updateexercice.ejs',
+                    { idexercices: exercices[0].idexercices, e_title: exercices[0].e_title, code_cours: exercices[0].code_cours, idstudent: exercices[0].idstudent, e_description: exercices[0].e_description });
+            }
+        });
+    };
+
+
+
+    /*exports.formulaireupdateexercice = function(req, res){
+        console.log('renvoi formulaire');
+        let idexercices = req.params.idexercices;
+        let sql = "SELECT * FROM exercices WHERE `exercices`.`idexercices` = ?";
         connection.query(sql, idexercices, function(err, data){
             if(err){
                 res.status(400).send(err);
@@ -53,29 +74,42 @@ exports.exercices = function(req, res){
             else{
                 res.status(202);
                 console.log(data);
+                listExercices = data;
                 res.render('updateexercice.ejs', {listExercices:data});
             }
         });
-    };
-
+    };*/
 
     //Route update
     exports.updateexercice = function (req, res) {
+        let exercice = new Exercices(req.body.e_title, req.body.code_cours, req.body.idstudent, req.body.e_description);
+        console.log(exercice);
+        connection.query("UPDATE exercices SET ? WHERE exercices.idexercices = ?",
+        [exercice, req.body.id], function (error, data) {
+            if (error) {
+                console.log(error);
+                res.status(400).send(error);
+            } else {
+                res.status(202).redirect('/accueil/exercices');
+            }
+        })
+    };
+
+
+    /*exports.updateexercice = function (req, res) {
         let exercices = new Exercices(req.body.e_title, req.body.code_cours, req.body.idstudent, req.body.e_description);
-        let sql = "UPDATE exercices SET ? WHERE exercices.idexercices = ?";
-        connection.query(sql, exercices, function(err, data){
+        connection.query("UPDATE exercices SET ? WHERE `idexercices` = ?", exercices, function(err, data) {
             if(err) {
                 console.log('Erreur modification exercice');
                 res.status(400).send(err);
             }
             else{
-                res.status(202);
                 console.log('Modification exercice réussie');
                 console.log(data);
-                res.redirect('/accueil/exercices');
+                res.status(202).redirect('/accueil/exercices');
             };
         });        
-    };
+    };*/
 
 
 //Supprimer un élément de la liste des exercices
